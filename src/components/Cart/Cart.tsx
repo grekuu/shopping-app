32cae249bcf668d20613002d3fee157526f24990
grekuu <gracjanprusik@gmail.com>
2022-11-01 15:15:33 +0100

    shopping cart working

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,7 +1,30 @@
+import { useAppSelector } from "../../redux/hooks";
+import { getCart } from "../../redux/productsSlice";
 import "./Cart.scss";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cart = useAppSelector(getCart);
+
+  return (
+    <div>
+      {cart ? (
+        <div>
+          {cart.map((item) => {
+            return (
+              <div className="cart-items">
+                <div className="single-cart-item">
+                  <img src={item.image} alt={item.title} />
+                  {item.title}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        <div>Loading</div>
+      )}
+    </div>
+  );
 };
 
 export default Cart;
